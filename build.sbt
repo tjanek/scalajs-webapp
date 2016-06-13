a942d0ee16c00f5b671b76faa83f5fb0528bd870
Tomasz Janek <janek.tomasz@gmail.com>
2016-06-13 09:29:40 +0200

    plug jQuery

--- build.sbt
+++ build.sbt
@@ -7,3 +7,8 @@ version := "0.1"
 scalaVersion := "2.11.8"
 
 libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.0"
+libraryDependencies += "be.doeraene" %%% "scalajs-jquery" % "0.9.0"
+
+skip in packageJSDependencies := false
+jsDependencies +=
+  "org.webjars" % "jquery" % "2.1.4" / "2.1.4/jquery.js"
--- src/main/scala/pl/tjanek/scalajs/webapp/ScalaJsWebApp.scala
+++ src/main/scala/pl/tjanek/scalajs/webapp/ScalaJsWebApp.scala
@@ -1,25 +1,23 @@
 package pl.tjanek.scalajs.webapp
 
 import scala.scalajs.js.JSApp
-import org.scalajs.dom
-import dom.document
-
-import scala.scalajs.js.annotation.JSExport
+import org.scalajs.jquery.jQuery
 
 object ScalaJsWebApp extends JSApp {
   def main() : Unit = {
-    appendPar(document.body, "Hello World")
+    jQuery(setupUI _)
   }
 
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    val textNode = document.createTextNode(text)
-    parNode.appendChild(textNode)
-    targetNode.appendChild(parNode)
+  def appendPar(text: String): Unit = {
+    jQuery("body").append(s"<p>${text}</p>")
   }
 
-  @JSExport
+  def setupUI(): Unit = {
+    jQuery("#click-me-button").click(addClickedMessage _)
+    jQuery("body").append("<p>Hello World</p>")
+  }
+
   def addClickedMessage(): Unit = {
-    appendPar(document.body, "You clicked the button!")
+    appendPar("You clicked the button!")
   }
 }
